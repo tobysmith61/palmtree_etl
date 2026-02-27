9d2abb2b66747b017a095e7a06b8a5a3ce6461d1
tobysmith61 <tobysmith61@hotmail.com>
2026-02-27 22:44:37 +0000

    celery tweeks

--- palmtree_etl/celery.py
+++ palmtree_etl/celery.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
+import tenants.tasks
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "palmtree_etl.settings")
 
--- tenants/tasks.py
+++ tenants/tasks.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 import shutil
+from celery import shared_task
 
 DROP_FOLDER = Path("/srv/sftp_drops/voltaris/777/drop")
 READY_FOLDER = Path("/srv/sftp_drops/voltaris/777/ready")
 READY_FOLDER.mkdir(exist_ok=True)  # ensure folder exists
 
+@shared_task
 def promote_dropzone_files():
     for file_path in DROP_FOLDER.iterdir():
         if file_path.is_file():
             dst = READY_FOLDER / file_path.name
             shutil.copy2(file_path, dst)  # copy preserves metadata
             print(f"Copied {file_path.name} to ready")
-            
