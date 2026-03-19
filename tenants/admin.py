b47ea6ae3c4a9421b19a2db5844744eb7edc9c89
tobysmith61 <tobysmith61@hotmail.com>
2026-03-19 21:49:11 +0000

    add job 'move_source_file_on_completion' to admin

--- tenants/admin.py
+++ tenants/admin.py
@@ -287,7 +287,7 @@ class AccountJobAdmin(
     fieldsets = (
         (None, {
             'fields':
-                ('account', 'job', 'sftp_drop_zone', 'tenant_mapping', 'account_table_data', 'auto_or_manual'),
+                ('account', 'job', 'sftp_drop_zone', 'tenant_mapping', 'account_table_data', 'auto_or_manual', 'move_source_file_on_completion'),
         }),
     )
 
